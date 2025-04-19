813378d1595f820d8924e478266d874e72e06254
AnthonyBasdfg <Anthony@Barrezueta.me>
2025-04-19 16:37:14 -0500

    Routes static cloudflare

---
+++ scripts/copy-routes.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const routesConfig = {
+  "version": 1,
+  "include": ["/*"],
+  "exclude": [
+    "/build/_assets/*",
+    "/*.ico",
+    "/*.svg", 
+    "/*.png",
+    "/*.jpg",
+    "/*.jpeg", 
+    "/*.gif"
+  ]
+};
+
+const buildDir = path.join(__dirname, '../build');
+const routesPath = path.join(buildDir, '_routes.json');
+
+// Asegurarse de que la carpeta build existe
+if (!fs.existsSync(buildDir)) {
+  console.error('¡La carpeta build no existe!');
+  process.exit(1);
+}
+
+// Escribir el archivo _routes.json
+fs.writeFileSync(
+  routesPath,
+  JSON.stringify(routesConfig, null, 2)
+);
+
+console.log('_routes.json generado correctamente en la carpeta build');
